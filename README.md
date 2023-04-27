6949e01bb2eaf353851b6bb1b9f328fab0b1bcfd
markfarkas-camunda <mark.farkas@camunda.com>
2023-04-27 16:33:47 +0200

    feat: add README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+# Email sender
+
+This is an example Spring Boot 3 project for sending emails using Gmail's SMTP server with a Gmail account.
+
+## Prerequisites
+
+1. Get an App Password as described here: [https://support.google.com/mail/answer/185833](https://support.google.com/mail/answer/185833)
+2. Replace the specific arguments in [EmailSender.java](src/main/java/io/camunda/email/EmailSender.java)
+
+## Build
+
+```bash
+mvn clean package
+```
+
+## Run
+
+Navigate to [EmailSenderApplication.java](src/main/java/io/camunda/email/EmailSenderApplication.java) and run it with your IDE.
+
--- src/main/java/io/camunda/email/EmailSender.java
+++ src/main/java/io/camunda/email/EmailSender.java
@@ -9,6 +9,7 @@ public class EmailSender {
     public void send(String to, String subject, String text) {
         // create email message
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("{{REPLACE_THIS_WITH_YOUR_EMAIL}}");
         simpleMailMessage.setTo(to);
         simpleMailMessage.setSubject(subject);
         simpleMailMessage.setText(text);
@@ -17,8 +18,8 @@ public class EmailSender {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
         javaMailSender.setHost("smtp.gmail.com");
         javaMailSender.setPort(587);
-        javaMailSender.setUsername("");
-        javaMailSender.setPassword("");
+        javaMailSender.setUsername("{{REPLACE_THIS_WITH_YOUR_EMAIL}}");
+        javaMailSender.setPassword("{{REPLACE_THIS_WITH_YOUR_APP_PASSWORD}}");
 
         // add properties
         Properties properties = javaMailSender.getJavaMailProperties();
